58899592e3e577ea5773d6cacd2342e1b61201d8
Tender666 <qwecisammy@gmail.com>
2026-03-21 18:08:48 +0000

    cors added

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    // credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application running on: http://localhost:${process.env.PORT ?? 4000}`);
+  console.log(
+    `Application running on: http://localhost:${process.env.PORT ?? 4000}`,
+  );
 }
 bootstrap();
