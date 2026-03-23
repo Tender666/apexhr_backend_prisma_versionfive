6259d93a3f06d0ef0750073b852c410b5a90a46d
Tender666 <qwecisammy@gmail.com>
2026-03-23 00:01:38 +0000

    dtos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TenantModule } from './tenant/tenant.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TenantModule],
+  imports: [TenantModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
